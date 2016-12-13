f396cebaba6b381ff0ab62f84495b9336a9bf21a
simplay <silent.simplay@gmail.com>
2016-12-13 19:11:52 +0100

    Bugfix: Setting the correct project name in the xml file

--- xml_generator.rb
+++ xml_generator.rb
@@ -1,5 +1,4 @@
 #!/usr/bin/env ruby
-
 # usage: `ruby parser.rb`
 #
 # generate an xml file for each repository contained in `./repos/`, containing the git log.
@@ -7,11 +6,16 @@
 out_path = Dir.pwd + "/out/"
 repository_sources = Dir["repos/*"]
 
-process_git_tree_cmd = "git log --pretty=format:\" <change>%n <author_name>%an</author_name>%n
-<author_e_mail>%ae</author_e_mail>%n <author_date>%ad</author_date>%n
-<committer_name>%cn</committer_name>%n <committer_email>%ce</committer_email>%n
-<committer_date>%cd</committer_date>%n <project>kernel_samsung</project>%n
-<subject>%s</subject>%n </change>\""
+# @example git_log_cmd_for("kernel_samsung")
+# @param project_name [String] name of project
+def git_log_cmd_for(project_name)
+  process_git_tree_cmd = "git log --pretty=format:\" <change>%n <author_name>%an</author_name>%n
+  <author_e_mail>%ae</author_e_mail>%n <author_date>%ad</author_date>%n
+  <committer_name>%cn</committer_name>%n <committer_email>%ce</committer_email>%n
+  <committer_date>%cd</committer_date>%n <project>#{project_name}</project>%n
+  <subject>%s</subject>%n </change>\""
+end
+
 
 repository_sources.each do |repo_source|
   repo_name = repo_source.split("/").last
@@ -19,7 +23,7 @@ repository_sources.each do |repo_source|
     file << "<?xml version=\"1.0\"?>\n"
     file << "<changes>\n"
   end
-  system("cd ./#{repo_source} && #{process_git_tree_cmd} >> #{out_path}/#{repo_name}.xml")
+  system("cd ./#{repo_source} && #{git_log_cmd_for(repo_name)} >> #{out_path}/#{repo_name}.xml")
   open("#{out_path}/#{repo_name}.xml", 'a') do |file|
     file << "\n"
     file << "</changes>"
